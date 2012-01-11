fce956c7e3baa7f43296aa0fe13e2a487d2de607
risk.limits <risk.limits@046cadc5-53c4-9616-ea9d-4bb1893bba41>
2012-01-11 22:22:55 +0000

    First working example of taking a file, storing it, and retrieving it.

--- src/lib/GridDocTransform.py
+++ src/lib/GridDocTransform.py
@@ -1,10 +1,57 @@
 """
-Basic class for working with files that are part of a GridFS file system
+The naming convention of files starting with the original
+    'Much Ado About Nothing by Shakespeare.docx'
+    'Much Ado About Nothing by Shakespeare.docx.txt'
+    'Much Ado About Nothing by Shakespeare.docx.pdf'
+    'Much Ado About Nothing by Shakespeare.docx.pdf.1.png'
+    'Much Ado About Nothing by Shakespeare.docx.pdf.2.png'
 """
 
 import pymongo
 import gridfs
-import win32com
+import win32com as win32
+import mimetypes
+import os
+import hashlib
+
+class FileChurner(object):
+    def __init__(self,gridFSobj,temporary_directory):
+        self.gridFSobj = gridFSobj
+        self.temporary_directory = temporary_directory
+
+    def process_file(self, filename):
+        hashed_filename = hashlib.sha1(filename).hexdigest()
+        writing_location = os.path.join(self.temporary_directory,hashed_filename)
+#        if os.path.exists(writing_location):
+#            os.rmdir(writing_location)
+#        os.mkdir(writing_location)
+
+        grid_out = self.gridFSobj.get_last_version(filename)
+        filename_to_write = os.path.join(writing_location,filename)
+        file_to_write = open(filename_to_write,'wb')
+
+        print(grid_out.filename)
+        print(grid_out.length)
+
+        for chunk in grid_out:
+            file_to_write.write(chunk)
+        file_to_write.close()
+
+
+        #os.rmdir(writing_location)
+
+    def process_all_files(self):
+        file_names = self.gridFSobj.list()
+        for filename in file_names:
+            self.process_file(filename)
+
+
+
+
+
+
+
+
 
 
 """
@@ -20,6 +67,16 @@ In [57]: doc = word.Documents.Open("C:\\users\\janos\\workspace\\grid-doc-transf
 In [58]: word.ActiveDocument.ExportAsFixedFormat("C:\\users\\janos\\Desktop\\sample2.pdf",17)
 In [59]: word.ActiveDocument.SaveAs("C:\\users\\janos\\Desktop\\sample2.txt",FileFormat = win32com.client.constants.wdFormatTextLineBreaks)
 In [60]: word.Quit()
+"""
+
+"""
+Powerpoint:
+In [1]: import win32com.client as win32
+In [2]: powerpoint = win32.gencache.EnsureDispatch("PowerPoint.Application")
+In [3]: powerpoint.Visible = True
+In [4]: presentation = powerpoint.Presentations.Open("C:\\users\\janos\\workspace\\grid-doc-transform\\src\\test\\Sample Presentation Plain Background.pptx")
+In [5]: powerpoint.Presentations.Application.ActivePresentation.SaveAs("C:\\users\\janos\\Desktop\\sample5.pdf",32)
+
 """
 
 """
@@ -50,11 +107,10 @@ In [10]: j.saveAs("C:\\users\\janos\\Desktop\\testpdf\\test.tif","com.adobe.acro
 """
 
 """
-Powerpoint:
-In [1]: import win32com.client as win32
-In [2]: powerpoint = win32.gencache.EnsureDispatch("PowerPoint.Application")
-In [3]: powerpoint.Visible = True
-In [4]: presentation = powerpoint.Presentations.Open("C:\\users\\janos\\workspace\\grid-doc-transform\\src\\test\\Sample Presentation Plain Background.pptx")
-In [5]: powerpoint.Presentations.Application.ActivePresentation.SaveAs("C:\\users\\janos\\Desktop\\sample5.pdf",32)
+Create a temporary directory based on a sha1 hexdigest
 
+import sha
+In [69]: s = sha.new("ddd")
+In [70]: os.path.join("C:\\temp\\",s.hexdigest())
+Out[70]: 'C:\\temp\\9c969ddf454079e3d439973bbab63ea6233e4087'
 """
