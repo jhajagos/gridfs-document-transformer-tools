04dcdf3e9d2223a8ae29c84cda72dff1e14901f9
somsatpathy@gmail.com <somsatpathy@gmail.com@046cadc5-53c4-9616-ea9d-4bb1893bba41>
2012-11-14 19:53:18 +0000

    Added logic to deal with document updates

--- src/LoadDocumentsFromDirectory.py
+++ src/LoadDocumentsFromDirectory.py
@@ -7,32 +7,57 @@ import gridfs
 import os
 import mimetypes
 import glob
+import datetime
+import time
+import ProcessNewDocumentsInGridFS
 
-def main(configuration,file_names):
+def main(configuration,file_names,path):
     grid_file_connection = pymongo.Connection(configuration["mongo_file_store"]["server_name"])
     grid_db = grid_file_connection[configuration["mongo_file_store"]["database_name"]]
     gfs = gridfs.GridFS(grid_db)
+    check_file_updates(configuration,file_names,gfs)
 
-    file_name_list = []
-    for file_name in file_names:
-        if "*" in file_name:
-            file_names_expanded = glob.glob(file_name)
-            for file_name_expanded in file_names_expanded:
-                file_name_list.append(file_name_expanded)
+
+#524: detect modified files
+def check_file_updates(configuration,files,gfs):
+    modified_files = []
+    for file in files:
+        last_modified_ts = int(os.path.getmtime(os.path.join(path, file)))
+        file_name_gridfs = os.path.basename(file)
+        if(gfs.exists(filename=file_name_gridfs)):
+            f = gfs.get_last_version(file_name_gridfs)
+            f_ts = int(f.time_stamp)
+            if(last_modified_ts > f_ts):
+                f = open(os.path.join(path, file),"rb")
+                mime_type_file_name = mimetypes.guess_type(file)[0]
+                ts = time.time()
+                print("Uploading modified file '%s'" % file)
+                gfs.put(f, content_type = mime_type_file_name, filename = file_name_gridfs, time_stamp = ts)
+                modified_files.append(file_name_gridfs)
         else:
-            file_name_list.append(file_name)
+            file_name_list = []
+            f = open(os.path.join(path, file),"rb")
+            mime_type_file_name = mimetypes.guess_type(file)[0]
+            print("Uploading '%s'" % file)
 
-    for file_name in file_name_list:
-        f = open(file_name,"rb")
-        file_name_gridfs = os.path.basename(file_name)
-        mime_type_file_name = mimetypes.guess_type(file_name)[0]
-        print("Uploading '%s'" % file_name)
-        gfs.put(f, content_type = mime_type_file_name, filename = file_name_gridfs)
-        f.close()
+            #524: add a timestamp field - used for checking file updates
+            ts = time.time()
+            gfs.put(f, content_type = mime_type_file_name, filename = file_name_gridfs, time_stamp = ts)
+            f.close()
+
+    #524: Call ProcessNewDocumentsInGridFS to do the churning and upload updated files into gridfs
+    if(len(modified_files)>0):
+        ProcessNewDocumentsInGridFS.process_files(modified_files,gfs,configuration["temporary_directory"])
+    else:
+        files = gfs.list()
+        ProcessNewDocumentsInGridFS.process_files(files,gfs,configuration["temporary_directory"])
+
 
 if __name__ == "__main__":
     config_name = os.path.join(os.path.split(os.path.realpath(__file__))[0],"config.json")
     f = open(config_name)
     config_json = f.read()
     config = json.loads(config_json)
-    main(config,sys.argv[1:])
+    path = 'C:/Users/janos/workspace/grid-doc-transform/src/test/sampledata'
+    files = os.listdir(path)
+    main(config,files,path)
